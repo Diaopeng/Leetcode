509f5dac2e5deaed2182f724b12f946f404945d2
Diaopeng <13061980179@163.com>
2017-04-14 10:58:24 +0800

    Problem 162, binary search

--- L162.java
+++ L162.java
@@ -23,3 +23,24 @@ public class Solution {
         return result;
     }
 }
+/*
+上面采用复杂度比较高的0(n)的方法，这里我们将采用效率更高的log(n)的方法
+题目最终right = left 退出循环，取得区域的最大值
+
+*/
+
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length -1;
+        while(right > left){
+            int mid1 = (left + right)/2;
+            int mid2 = mid1 + 1;
+            if(nums[mid1] > nums[mid2]) 
+            right = mid1;
+            else 
+            left = mid2;
+        }
+        return left;
+    }
+}
