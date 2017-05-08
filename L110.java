bc6060e41bf86e1ce0e8a5b45086014a5fe0ee45
Diaopeng <13061980179@163.com>
2017-05-08 19:01:23 +0800

    Problem 110, 递归二叉树

--- L110.java
+++ L110.java
@@ -29,3 +29,36 @@ public class Solution {
         return deep;
     }
 }
+
+/*
+C：这个解法确实很强，在递归计算高度的时候就计算得到最终的结果 
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+        
+    }
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
+        
+        int left = dfs(root.left);
+        if(left == -1) return -1;
+        int right = dfs(root.right);
+        if(right == -1) return -1;
+        
+        if(Math.abs(left-right) > 1) return -1;
+        return Math.max(left,right) + 1;
+    }
+   
+}
