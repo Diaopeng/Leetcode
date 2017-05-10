f96153f397dccf318347dd384a86b2ff7bc497db
Diaopeng <13061980179@163.com>
2017-05-10 20:02:51 +0800

    Problem 541,简单string

--- L541.java
+++ L541.java
@@ -5,8 +5,6 @@ StringBuffer或者charArray的，这样才可以进行更改
 */
 
 
-
-
 public class Solution {
     public String reverseStr(String s, int k) {
         StringBuffer temp = new StringBuffer();
@@ -22,5 +20,27 @@ public class Solution {
             i += 2*k;
         }
         return result.toString();
+    }
+}
+//这个题目也可以转化成简单charArray  下面是答案的做法
+
+public class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        while(i < n) {
+            int j = Math.min(i + k - 1, n - 1);
+            swap(arr, i, j);
+            i += 2 * k;
+        }
+        return String.valueOf(arr);
+    }
+    private void swap(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
     }
 }
