3fb67c1874c9c42ebdbafa1d6844293a337021b1
Diaopeng <13061980179@163.com>
2017-05-23 23:36:36 +0800

    Problem 48,简单地数组问题

--- L48.java
+++ L48.java
@@ -1,6 +1,7 @@
 /*
 leetcode problem 48
 题目对于搞过数字图像处理的我来说还是比较简单，但是这样的话复杂度就是2n^2，能不能继续优化一下呢
+这个题目要注意一下空间的复杂度，重新开n^2内存不太合适，也不符合follow up的要求
 */
 
 public class Solution {
@@ -15,6 +16,27 @@ public class Solution {
         for( int i = 0; i < n; i++){
             for( int j = 0;j < n;j++){
                 matrix[i][j] = temp[i][j];
+            }
+        }
+    }
+}
+//follow up  can you do it in-place?
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for( int i = 0; i < n; i++){
+            for( int j = i;j < n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for( int i = 0; i < n/2; i++){
+            for( int j = 0;j < n;j++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
             }
         }
     }
