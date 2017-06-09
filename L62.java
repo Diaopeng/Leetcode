6eec4b1c3f81313420fbdfd32d88a63cda6c1a0b
Diaopeng <13061980179@163.com>
2017-06-09 19:24:36 +0800

    Problem 62,简单地动态规划

---
+++ L62.java
@@ -0,0 +1,21 @@
+/*
+简单地动态规划问题，状态转移方程dp[i][j] = dp[i][j-1] + dp[i-1][j];
+*/
+
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][] result = new int[m][n];
+        for(int i = 0;i < n;i++){
+            result[0][i] = 1;
+        }
+        for(int i = 0;i < m;i++){
+            result[i][0] = 1;
+        }
+        for(int i = 1; i < m ;i++){
+            for( int j = 1; j < n;j++){
+                result[i][j] = result[i-1][j] + result[i][j-1];
+            }
+        }
+        return result[m-1][n-1];
+    }
+}
