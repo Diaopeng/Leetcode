06f44ed36ad24a98f83f03c824e5316149bd5cbc
Diaopeng <13061980179@163.com>
2017-06-19 18:43:10 +0800

    Problem 343

--- L343.java
+++ L343.java
@@ -16,3 +16,22 @@ public class Solution {
         
     }
 }
+
+//答案的解法就是将n不断的分解，因为n%2 == 0  n >= 4时候是可以分解的,n是奇数时候，n >= 5分解是合算的，所以选择2或者3进行分解，
+
+public class Solution {
+    public int integerBreak(int n) {
+        if(n == 2)
+            return 1;
+        if(n == 3)
+            return 2;
+        int result = 1;
+        while(n > 4){
+            result *= 3;
+            n -= 3;
+        }
+        result = result * n;
+        return result;
+        
+    }
+}
