e81f9c52feefffd38d0eb357d5c171a475c5fdad
Diaopeng <13061980179@163.com>
2017-08-12 16:11:01 +0800

    空间复杂度优化

--- L119.java
+++ L119.java
@@ -20,3 +20,18 @@ public class Solution {
 		return result;
     }
 }
+
+//我们降低空间复杂度，将空间复杂度降低至O(k)
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 0;i < rowIndex + 1; i++){
+            result.add(1);
+            for(int j = i - 1;j >= 1;j--){
+                result.set(j, result.get(j) + result.get(j-1));
+            }
+        }
+        return result;
+            
+    }
+}
