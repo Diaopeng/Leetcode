3b9c5eb46019ee7a580c95cd8deec31a4c247498
Diaopeng <13061980179@163.com>
2017-08-24 10:59:33 +0800

    改进版

--- L15.java
+++ L15.java
@@ -27,3 +27,26 @@ public class Solution {
         return result;
     }
 }
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer> >result = new ArrayList<List<Integer> >();
+        Arrays.sort(nums);
+        for(int i = 0;i < nums.length - 2;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(nums[i] > 0) break;
+            int small = i + 1,big = nums.length - 1, target = - nums[i];
+            while(small < big){
+                if(nums[small] + nums[big] == target){
+                    result.add(Arrays.asList(nums[i],nums[small],nums[big]));
+                    while(small < big && nums[small] == nums[small + 1]) small ++;
+                    while(small < big && nums[big - 1] == nums[big]) big --;
+                    small ++; big --;
+                }
+                else if(nums[small] + nums[big] > target) big --;
+                else small ++;
+            }
+        }
+        return result;
+    }
+}
