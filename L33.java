146158a4109348cd11b5a0b1caae77748253b820
Diaopeng <13061980179@163.com>
2018-01-18 21:40:09 -0800

    binary search

---
+++ L33.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        while(left+1 < right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > nums[left]){
+                if(target >= nums[left] && target <= nums[mid]) right = mid;
+                else  left = mid;
+            }
+            else if(nums[mid] < nums[right]){
+                if(target >= nums[mid] && target <= nums[right]) left = mid;
+                else right = mid;
+            }
+        }
+        if(nums[left] == target) return left;
+        if(nums[right] == target) return right;
+        return -1;
+    }
+}
